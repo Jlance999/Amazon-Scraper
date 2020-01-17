e3b1e161de0874ac3b617cc3ab1df4963cce5869
Jlance999 <jeremy.perkins99@gmail.com>
2020-01-17 01:10:32 -0500

    Image Functionality!
    check_price(): Changes
    Added filtering to locate and save the image url of the landing image to a variable. Used requests to download said image to the premade directory.
    
    GUI(): Changes
    Added drop down menu which gets its selectioins from a list, currently working on getting the selections to autopopulate the rest of the GUI with the product name, price, image and graph.
    
    Hard coded the first image I've downloaded to figure out how to properly incorporate it into the GUI, had to add another inport to get image handling working but the image is now fully integrated into the gui and is auto-resizing when the program is stretched/maximized.

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -15,7 +15,7 @@ import os
 mainDir = "C:\\Users\\Jeremy\\Documents\\GitHub\\Amazon Scraper\\"
 
 def check_price():
-
+    
     URL = 'https://www.amazon.com/Synology-DS418play-Station-4-bay-Diskless/dp/B075ZNKCK4/ref=cm_cr_arp_d_product_sims?ie=UTF8'
 
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
@@ -23,23 +23,50 @@ def check_price():
     page = requests.get(URL, headers=headers)
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup)
+    
+    # for use in debugging 
+    # print(soup)
 
     soup2 = BeautifulSoup(soup.prettify(), "html.parser")
 
+    #Scraping title and price of product from parser.
     title = soup2.find(id= "productTitle").get_text()
     price = soup2.find(id= "priceblock_ourprice").get_text()
     converted_price = Decimal(price.strip('$'))
     stripped_title = title.strip()
+    
+    # Filtering down to the closs that holds all of the image information
+    imgs = soup2.find('div', class_='imgTagWrapper')
+    # For debugging print('imgs= ',imgs)
+
+
+    # Filtering down to the direct product image url.
+    imgsurl = imgs.find('img', class_='a-dynamic-image') ['data-a-dynamic-image']
+    imgsurl = imgsurl.split('"')
+    imgsurl = imgsurl[1]
+    print('imgsurl= ',imgsurl)
+
+
 
     if(converted_price < 425):
         send_mail()
 
     print(converted_price)
     print(title.strip())
-
+    
     setupDir(str(stripped_title))
     storeDataTxt(str(stripped_title),str(converted_price))
+    storeImagePng(str(stripped_title),imgsurl)
+
+
+def storeImagePng (product,imgsurl):
+
+    r = requests.get(imgsurl)
+
+    imageDataDir=mainDir + '//' + product + '//'
+    imageData = open(imageDataDir+'image.gif', "wb")
+    imageData.write(r.content)
+    print('Image saved to Directory.')
 
 def storeDataTxt (product,price):
     #Toying with storing data to txt files. Not sure if I will use this or another method, but I want to be able to see price changes over time.
@@ -56,14 +83,16 @@ def storeDataTxt (product,price):
 
 def setupDir(product):
     
-    if (os.path.isdir('./'+ product) ==False):
+    if (os.path.isdir(mainDir + '//' + product) ==False):
         os.mkdir(mainDir + product)
         os.mkdir(mainDir + product + '//' +'productImages')
-        
         titleDataDir=mainDir + '//' + product + '//' +"productData.txt"
         titleData = open(titleDataDir, "a")
         titleData.write(product)
         titleData.close()
+
+    else:
+        print("Directory already exists.")
 
 def graph():
     style.use('dark_background')
@@ -120,21 +149,57 @@ def send_mail():
     print('Email has been sent.')
 
 def GUI ():
+    from PIL import Image, ImageTk
 
     root = tk.Tk()
+    root.title("Amazon Scraperino")
     cHeight=500
     cWidth=800
 
     canvas = tk.Canvas(root, height=cHeight, width=cWidth)
     canvas.pack()
 
+    
+    # Master frame
     frame = tk.Frame(root, bg='#1f1f1f')
     frame.place(relwidth=1, relheight=1)
 
-    #Frame to hold image label
+    # List to hold currently indexed products
+    productList = [
+        'Synology DS418play',
+        'Sweet Cell Phone Case',
+        'Arduino'
+    ]
+
+    # Dropdown for product selection
+    var=tk.StringVar()          # Holds current selection
+    var.set(productList[0])        # Sets Default Selection
+
+    # Configuring actual drop down position.
+    productSel= tk.OptionMenu(frame, var, *productList)     #Configuring actual drop down
+    productSel.place(anchor='s', relx=.75, rely=.25)
+
+
+    # Frame to hold image label
     frameInner = tk.Frame(frame, bg= "#282828")
     frameInner.place(anchor='nw', relwidth=.4, relheight=.5)
 
+    def resize_image(event):
+        new_width = event.width
+        new_height = event.height
+        image = copy_of_image.resize((new_width, new_height))
+        photo = ImageTk.PhotoImage(image)
+        labelImage.config(image = photo)
+        labelImage.image = photo #avoid garbage collection
+
+    image = Image.open("C:\\Users\\Jeremy\\Documents\\GitHub\\Amazon Scraper\\image.png")
+    copy_of_image = image.copy()
+    photo = ImageTk.PhotoImage(image)
+    labelImage = tk.Label(frameInner, image=photo)
+    labelImage.bind('<Configure>', resize_image)
+    labelImage.pack()
+
+  
     #Stores command=function_name to run a function
     button = tk.Button(frame, text="See Graph", bg='gray', command=graph)
     button.place(anchor='s', relx=.5, rely=.99)
@@ -156,6 +221,7 @@ def GUI ():
     root.mainloop()
 
 GUI()
+
 ##
 ##while(True):
 ##    check_price()
